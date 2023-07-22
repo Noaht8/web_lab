a4759e47e0b667b9b906b8c95983a959d48a795d
Noah Tsegay <noahtsegay8@gmail.com>
2023-07-22 23:59:13 +0300

    Update All Files

---
+++ Learn JS/learn.js
@@ -0,0 +1,19 @@
+let myVariable;
+myVariable = "Noah";
+myVariable;
+// This is a comment
+let iceCream = "Vanilla";
+if (iceCream === "Vanilla"){
+    alert("Yay, I love Vanilla!");
+}
+else{
+    alert("aww, but Vanilla is my favorite");
+}
+
+function multiply(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+
+multiply(4, 7);
+console.log('done');
